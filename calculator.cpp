042cb57ec676bb25b44d8109c363bb3c248d8288
avjumaquio <arniejumaquio@gmail.com>
2021-10-02 21:20:23 +0800

    Skip git add.Go to repo quickly

--- calculator.cpp
+++ calculator.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int firstNumber, secondNumber, sumOfTwoNumbers,diffOfTwoNumbers;
     
-    cout <<"Hello Venus!";
-    cout <<"Hello World!";
+  
     cout << "Enter two integers: ";
     cin >> firstNumber >> secondNumber;
 
     // sum of two numbers in stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
+    diffOfTwoNumbers=firstNumber-secondNumber;
 
     // Prints sum 
     cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    //Print difference
+    cout << firstNumber << " - " <<  secondNumber << " = " << diffOfTwoNumbers; 
 
     return 0;
 
